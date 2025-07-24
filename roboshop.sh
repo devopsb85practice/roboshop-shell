b51912e8f1f6960c64bb85257f304ab136deb17c
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-24 13:53:32 -0400

    practice

--- roboshop.sh
+++ roboshop.sh
@@ -6,11 +6,14 @@ ZONE_ID="Z06642261B4AEFFCIQJUH"
 DOMAIN_NAME="prasannadevops.online"
 for instance in ${INSTNACES[@]}
 do 
-aws ec2 run-instances \
-    --image-id ami-09c813fb71547fc4f \
-    --count 1 \
-    --instance-type t2.micro \
-    --security-group-ids sg-0ed01617e03501426 \
-	--tag-specifications 'ResourceType=instance,Tags=[{Key=Name, Value="$instance"}]'
-
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 \ --instance-type t2.micro --security-group-ids sg-0ed01617e03501426 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --Output text)	
+    if [$instance -ne "frontend"]
+    then
+        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
+        Instances[0].PrivateIpAddress" --output text
+    else
+        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
+        Instances[0].PublicIpAddress" --output text
+    fi
+    echo "$instance Ip address: $IP"
 done
