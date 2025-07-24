262a9cd8143e669c26809eb5fae8058195b391b3
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-24 14:04:51 -0400

    practice

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ ZONE_ID="Z06642261B4AEFFCIQJUH"
 DOMAIN_NAME="prasannadevops.online"
 for instance in ${INSTNACES[@]}
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 \ --instance-type t2.micro --security-group-ids sg-0ed01617e03501426 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)	
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 --instance-type t2.micro --security-group-ids sg-0ed01617e03501426 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)	
     if [$instance != "frontend"]
     then
         IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].
