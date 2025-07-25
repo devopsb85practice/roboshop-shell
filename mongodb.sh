875fbf62097b8f38c22636752f10a6a67ccf69ce
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-24 21:55:04 -0400

    practice

--- mongodb.sh
+++ mongodb.sh
@@ -9,30 +9,30 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="/$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "script started executing at $(date)"| tee -a $LOG_FILE
-if ["$USERID" -ne 0]
+if [ "$USERID" -ne 0 ]
 then   
-    echo "$R you are not running with root access, please run with root access $N" | tee -a $LOG_FILE
+    echo -e "$R you are not running with root access, please run with root access $N" | tee -a $LOG_FILE
 else
-    echo "$G you are running with root access $N" | tee -a $LOG_FILE
+    echo -e "$G you are running with root access $N" | tee -a $LOG_FILE
 fi
 VALIDATE(){
-if ["$1" -eq 0]
+if [ "$1" -eq 0 ]
 then
-    echo "$2 is $G success $N"
+    echo -e "$2 is $G success $N" | tee -a $LOG_FILE
 else
-    echo "$2 is $R failure $N"
+    echo -e "$2 is $R failure $N" | tee -a $LOG_FILE
     exit 1
 }
 fi
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "cppying the repo file"
-dnf install mongodb-org -y 
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "cppying the repo file" 
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "installing mongodb"
-systemctl enable mongod 
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "enabling mongodb"
-systemctl start mongod 
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "starting mongodb"
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.conf
-VALIDATE $? "editing mongo.conf file"
-systemctl restart mongod
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.conf &>>$LOG_FILE
+VALIDATE $? "editing mongo.conf file" 
+systemctl restart mongod &>>$LOG_FILE
 validate $? "restarting mongodb"
