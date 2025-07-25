fede7d6128d1dc80e20308b5d458d8c0a64bf714
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-24 22:06:38 -0400

    practice

--- mongodb.sh
+++ mongodb.sh
@@ -26,14 +26,14 @@ else
 fi
 }
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-VALIDATE $? "cppying the repo file" 
+VALIDATE $? "copying the repo file" 
 dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "installing mongodb"
 systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "enabling mongodb"
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "starting mongodb"
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.conf &>>$LOG_FILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
 VALIDATE $? "editing mongo.conf file" 
 systemctl restart mongod &>>$LOG_FILE
 validate $? "restarting mongodb"
