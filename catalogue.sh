621b5d7df63e131a599c8e30f490a48e399b2c9c
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-28 12:11:02 -0400

    practice

--- catalogue.sh
+++ catalogue.sh
@@ -33,7 +33,7 @@ VALIDATE $? "enabling nodejs"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "installing nodejs"
 id roboshop
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
 else
